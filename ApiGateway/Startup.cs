c6a711826798f3d979f647613d7339ce01b2551e
and1helicopter <and1helicopter@mail.ru>
2021-02-10 13:57:22 +0300

    v0.24
    +project was structured
    
    Signed-off-by: and1helicopter <and1helicopter@mail.ru>

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -3,13 +3,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using AeroSearchREST.Models;
 using Microsoft.OpenApi.Models;
-using System;
 using System.IO;
+using System;
 using System.Reflection;
+using Serilog;
 
-namespace ApiGateway
+namespace AeroSearchREST
 {
     public class Startup
     {
@@ -18,6 +20,8 @@ namespace ApiGateway
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -25,26 +29,45 @@ namespace ApiGateway
         {
             services.AddControllers();
 
-            //Подключение свагера
+            // Use a PostgreSQL database
+            var postgreConnectionString = Configuration.GetSection("PostgreSQL").GetSection("ConnectionStrings").Value;
+            services.AddDbContext<AeroSearchContext>(options => options.UseLazyLoadingProxies().UseNpgsql(postgreConnectionString));
+
+            //Singleton т.к. один кэш постянный на все приложение
+            services.Configure<RedisCacheConfiguration>(Configuration.GetSection("Redis"));
+            services.AddSingleton<IRedisCacheService, RedisCacheService>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:9001").AllowAnyHeader().AllowAnyMethod();
+                    });
+            });
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo 
-                { 
-                    Title = "ApiGateway AeroSearch", 
-                    Version = "v1",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "and1helicopter",
-                        Email = "and1helicopter1@gmail.com"                        
-                    }
-                });
-
-                // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "AeroSearch API", 
+                    Version = "v1",
+                    Description = "API to air search",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Denis Zaika",
+                        Email = "and1helicopter@mail.ru"
+                    }
+                });
             });
         }
+
+
+        //API token:     eb8a6267a4e794733a266041cc3d85c0
+        //Marker   :     243531
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -52,33 +75,28 @@ namespace ApiGateway
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
-            var loggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder.AddDebug();
-            });
-            ILogger logger = loggerFactory.CreateLogger<Startup>();
-
-            //Подключаю свагер
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("./swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
-            });
+            app.UseSerilogRequestLogging();
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
+            });            
         }
     }
 }
---
+++ Logger/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoggerService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet("shallow")]
+        public StatusCodeResult Shallow()
+        {
+            return new StatusCodeResult(200);
+        }
+    }
+}
